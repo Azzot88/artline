cc876d19a9d8eabe82fe77f15a155d50d6ebafc6
Nick <azzot88@gmail.com>
2026-01-25 20:05:35 -0500

    fix 1

--- app/models.py
+++ app/models.py
@@ -6,3 +6,4 @@ from app.domain.jobs.models import Job
 from app.domain.providers.models import ProviderConfig, AIModel
 from app.domain.users.guest_models import GuestProfile
 from app.domain.users.likes_model import Like
+from app.domain.pricing.models import PricingQuote
