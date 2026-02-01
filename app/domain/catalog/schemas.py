c4c53ee4017d6a352b5e4255a737252a075abd20
Nick <azzot88@gmail.com>
2026-01-31 23:40:20 -0500

    fix 5

--- app/domain/catalog/schemas.py
+++ app/domain/catalog/schemas.py
@@ -1,6 +1,6 @@
 
 from pydantic import BaseModel, ConfigDict, Field
-from typing import List, Optional, Any, Union, Literal
+from typing import List, Optional, Any, Union, Literal, Dict
 
 class ParameterOption(BaseModel):
     label: str
